7c6f8665af52bd7023ae4b40bb249a1c734c6fb4
12Sasha21 <90248554+12Sasha21@users.noreply.github.com>
2022-07-17 15:02:22 +0300

    commit

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts"); // імпорт групи маршрутів
 
-const app = express()
+const app = express(); // створення веб-сервера
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short"; // якщо режим розробника тоді виводити повну інфор.
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger)); // мідлвар для виводу данних
+app.use(cors());
+// app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
+app.use("/api/contacts", contactsRouter); // створення групи маршрутів. Будь який запит, що починається з /api/contacts, оброблюється - contactsRouterю
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" }); // якщо немає адреси
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,56 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "./contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactsId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactsId);
+  return !result ? "contact not found" : result;
+};
 
-const addContact = async (body) => {}
+// const removeContact = async (contactId) => {
+//   const contacts = await listContacts();
+//   const idx = contacts.findIndex((item) => item.id === contactId);
+//   if (idx === -1) {
+//     return null;
+//   }
+//   const [removeContact] = contacts.splice(idx, 1);
+//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//   return removeContact;
+// };
 
-const updateContact = async (contactId, body) => {}
+// const addContact = async (name, email, phone) => {
+//   const contacts = await listContacts();
+//   const id = `${Number(contacts[contacts.length - 1].id) + 1}`;
+//   const newContact = {
+//     id: id,
+//     name,
+//     email,
+//     phone,
+//   };
+//   contacts.push(newContact);
+//   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//   return newContact;
+// };
+
+// const getContactById = async (contactId) => {}
+
+// const removeContact = async (contactId) => {}
+
+// const addContact = async (body) => {}
+
+// const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+  // removeContact,
+  // addContact,
+  // updateContact,
+};
