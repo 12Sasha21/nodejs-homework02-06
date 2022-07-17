32770436b0e685a0d4457441f279b1623ba60a30
12Sasha21 <90248554+12Sasha21@users.noreply.github.com>
2022-07-17 17:25:11 +0300

    done

--- app.js
+++ app.js
@@ -8,18 +8,20 @@ const app = express(); // створення веб-сервера
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short"; // якщо режим розробника тоді виводити повну інфор.
 
-app.use(logger(formatsLogger)); // мідлвар для виводу данних
+app.use(logger(formatsLogger)); // мідлвар для виводу данних в консоль
 app.use(cors());
-// app.use(express.json());
+app.use(express.json()); // якщо content-type це json, тоді парсить його та зберігаємо в req.body
 
-app.use("/api/contacts", contactsRouter); // створення групи маршрутів. Будь який запит, що починається з /api/contacts, оброблюється - contactsRouterю
+app.use("/api/contacts", contactsRouter); // створення групи маршрутів. Будь який запит, що починається з /api/contacts, оброблюється - contactsRouter
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" }); // якщо немає адреси
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  // піймана помилка із роута - четвертий параметр(err)
+  const { status = 500, message = "Server error" } = err; // якщо немає статусу тоді 500
+  res.status(status).json({ message }); // якщо немає меседжу тоді "Server error"
 });
 
 module.exports = app;
